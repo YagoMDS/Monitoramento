2086aea4ea4934ed8c6d51a42a7402613355a801
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2025-03-06 22:40:58 -0300

    Update
    Ajustes necessários devido a separação da lógica do arquivo main.

--- main.py
+++ main.py
@@ -2,25 +2,11 @@ import requests
 import time
 import os
 import psycopg2
+from database import conectar_db
 from dotenv import load_dotenv 
 
 # Carregar o arquivo .env
 load_dotenv("infos.env")
-
-# def conectar_db():
-
-#     try:
-#         conn = psycopg2.connect(
-#             dbname=os.getenv("DB_NAME"),
-#             user=os.getenv("DB_USER"),
-#             password=os.getenv("DB_PASSWORD"),
-#             host=os.getenv("DB_HOST"),
-#             port=os.getenv("DB_PORT"),
-#         )
-#         return conn
-#     except Exception as e:
-#         print(f"Erro na conexão com o banco de dados: {e}")
-#         return None
 
 # Gera um novo Token para consulta na API
 def GerarToken():
@@ -88,27 +74,27 @@ def ConsultarPrecoPromoc(produto_id, access_token):
         return None
     
 # Insere dados na tabela 
-# def insert(*args):
+def insert(*args):
 
-#     conexao = conectar_db()
-#     if not conexao:
-#         return
+    conexao = conectar_db()
+    if not conexao:
+        return
 
-#     try:
-#         cur = conexao.cursor()
+    try:
+        cur = conexao.cursor()
 
-#         query = "INSERT INTO precos(id_produto, price_default, price_promotion, last_updated) VALUES (%s, %s, %s, %s)" 
-#         cur.execute(query, args)
+        query = "INSERT INTO precos(id_produto, price_default, price_promotion, last_updated) VALUES (%s, %s, %s, %s)" 
+        cur.execute(query, args)
 
-#         conexao.commit()  # Confirma a transação
-#         cur.close()
-#         conexao.close()
+        conexao.commit()  # Confirma a transação
+        cur.close()
+        conexao.close()
 
-#         return print("Inserção realizada com sucesso!")
+        return print("Inserção realizada com sucesso!")
     
-#     except psycopg2.Error as e:
-#         print(f"Erro ao inserir dados: {e}")
-#         return None
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir dados: {e}")
+        return None
     
 
 def main():
